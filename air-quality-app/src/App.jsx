2fd379435934e2f42431e94d982a555f734c7e66
amuari <work.derrickabbey@gmail.com>
2024-05-08 09:41:36 +0000

    first commit

---
+++ air-quality-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+const getAirQuality = async (city) => {
+  try {
+    const response = await fetch(
+      `https://api.waqi.info/feed/${city}/?token=${REACT_APP_AQI_AOI_TOKEN}`
+    )
+    const data = response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+function App() {
+  return <main></main>
+}
+
+export default App
