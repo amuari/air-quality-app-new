6d7112aa69997c01d85325c22f5e1401162baebd
amuari <work.derrickabbey@gmail.com>
2024-05-08 16:06:46 +0000

    api done

--- air-quality-app/src/App.jsx
+++ air-quality-app/src/App.jsx
@@ -1,17 +1,40 @@
 import './App.css'
-const getAirQuality = async (city) => {
-  try {
-    const response = await fetch(
-      `https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_AOI_TOKEN}`
-    )
-    const data = await response.json()
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
+import { useState } from 'react'
+import CitySearch from './components/CitySearch'
+
 function App() {
-  return <main>hello world</main>
+  const [error, setError] = useState(null)
+  const [airQualityData, setAirQualityData] = useState(null)
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(
+        `https://api.waqi.info/feed/${city}/?token=245a49a132fae6fdd9a014ee209a01efe0e0855f  `
+      )
+      const data = await response.json()
+      console.log(data)
+      if (response.ok && data.status === 'ok') {
+        setAirQualityData(data.data)
+        setError(null)
+      } else {
+        setError(
+          'Sorry city not available. Try another location nearby or check your spelling  '
+        )
+        setAirQualityData(null)
+      }
+    } catch (error) {
+      console.error('network error:', error)
+      setError('Sorry,Something went wwrong')
+      setAirQualityData(null)
+    }
+  }
+  return (
+    <main>
+      <h1 className='text-3xl font-semibold text-gray-800  lg:text-4xl'>
+        Air Quality Index Checker
+      </h1>
+      <CitySearch getAirQuality={getAirQuality} />
+    </main>
+  )
 }
 
 export default App
---
+++ air-quality-app/src/components/CitySearch.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const CitySearch = ({ getAirQuality }) => {
+  const [inputValue, setInputValue] = useState('')
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    const formattedCity = inputValue.replace(/ /g, '-')
+    getAirQuality(formattedCity)
+  }
+  return (
+    <div>
+      <form action=''>
+        <input
+          type='search'
+          placeholder='Enter city'
+          value={inputValue}
+          onChange={handleInputChange}
+          className='px-4 py-2 text-gray-700 bg-white border rounded-md sm:mx-2 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40'
+        ></input>
+        <button
+          type='submit'
+          onClick={handleSearch}
+          className='x-4 py-2 m-2 p-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-md sm:mx-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CitySearch
