2aee5deeb0a631d9afeaa7ae0ac7f417593ebbca
amuari <work.derrickabbey@gmail.com>
2024-05-08 09:54:49 +0000

    env

--- air-quality-app/src/App.jsx
+++ air-quality-app/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 const getAirQuality = async (city) => {
   try {
     const response = await fetch(
-      `https://api.waqi.info/feed/${city}/?token=${REACT_APP_AQI_AOI_TOKEN}`
+      `https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_AOI_TOKEN}`
     )
     const data = await response.json()
     console.log(data)
